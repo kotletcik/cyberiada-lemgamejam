85f823537a19cfae294f6668d9d88af77b8424b9
PokyGrey <pdjurczyk@gmail.com>
2026-04-17 06:13:51 +0200

    a

--- Scripts/PJ/Aliens_actions_manager.gd
+++ Scripts/PJ/Aliens_actions_manager.gd
@@ -4,10 +4,12 @@ class_name Aliens_actions_manager
 @export var dances: Array[Alien_dance]
 
 @export var aliens: Array[Alien]
+var current_dance: Alien_dance
 var current_aliens_path: Array[Vector2]
 var current_dance_name: String
-var current_dance_movement_speed
+var is_static: bool
 static var instance: Aliens_actions_manager
+
 
 func _ready():
 	if (instance != null):
@@ -16,21 +18,23 @@ func _ready():
 	instance = self
 	EventBus.step_is_past.connect(give_action_to_aliens)
 	EventBus.light_is_switched.connect(change_dance_randomly)
-	change_dance_randomly()
+	change_dance_randomly(true)
 	
 func give_action_to_aliens():
 	for alien in aliens:
 		#var alien_target_pos_index = ((alien.id + 1) * current_aliens_path.size() / aliens.size() + (alien.id + 1) * Cycle_manager.instance.current_time_step) % current_aliens_path.size()
-		var alien_target_pos_index = ((alien.id + 1) + Cycle_manager.instance.general_step_counter * current_dance_movement_speed) % current_aliens_path.size()
-		alien.current_alien_action = Alien_action.new(current_aliens_path[alien_target_pos_index], current_dance_name)
-		alien.speed = current_dance_movement_speed
+		var alien_target_pos_index = ((alien.id + 1) + Cycle_manager.instance.general_step_counter * int(is_static)) % current_aliens_path.size()
+		alien.is_static = is_static
 		#print ("alien target pos index", alien_target_pos_index)
 		alien.do_current_action()
 
-func change_dance_randomly():
+func change_dance_randomly(empty: bool):
 	var number = randi_range(0, dances.size() - 1)
 	var new_dance = dances[number]
 	print (number)
 	current_aliens_path = new_dance.path
 	current_dance_name = new_dance.dance_name
-	current_dance_movement_speed = new_dance.moving_speed
+	is_static = new_dance.is_static
+
+func change_dance_to_next():
+	var index = (dances.find(current_dance) + 1) % dances.size()
--- Scripts/PJ/Event_bus.gd
+++ Scripts/PJ/Event_bus.gd
@@ -3,17 +3,20 @@ class_name Event_bus
 
 signal step_is_past()
 signal light_is_switched(to_day: bool)
+signal day_static_dance_started()
 
 enum triggers
 {
 	None,
 	step_is_past,
-	light_is_switched
+	light_is_switched,
+	day_static_dance_started
 }
 
 func reset_signal_subscribers():
 	_disconnect_all("step_is_past")
 	_disconnect_all("light_is_switched")
+	_disconnect_all("day_static_dance_started")
 
 func _disconnect_all(signal_name: String):
 	var connections = get_signal_connection_list(signal_name)
@@ -27,6 +30,8 @@ func call_event(trigger: triggers):
 			step_is_past.emit();
 		triggers.light_is_switched:
 			light_is_switched.emit();
+		triggers.day_static_dance_started:
+			day_static_dance_started.emit();
 		triggers.None:
 			return;
 	
